44d9f636095a07693e431b731d213eeda0421d9b
Roman Kotyshev <roman.kotyshev@yandex.ru>
2025-02-20 17:24:22 +0300

    DF-1: adjust dictionary cards, use custom css variables as flexible approach to set width and height properties

--- src/app/features/dictionaries/components/dictionary/dictionary.component.ts
+++ src/app/features/dictionaries/components/dictionary/dictionary.component.ts
@@ -1,11 +1,18 @@
 import { Component, input } from '@angular/core';
 import { Dictionary } from '../../models/dictionary.model';
-import { TuiCardLarge } from '@taiga-ui/layout';
+import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
+import { TitleDirective } from '../../../../shared/directives/title/title.directive';
+import { HeaderDirective } from '../../../../shared/directives/header/header.directive';
+import { TuiAppearance } from '@taiga-ui/core';
 
 @Component({
   selector: 'df-dictionary',
   imports: [
-    TuiCardLarge
+    TuiCardLarge,
+    TuiHeader,
+    TitleDirective,
+    HeaderDirective,
+    TuiAppearance
   ],
   templateUrl: './dictionary.component.html',
   styleUrl: './dictionary.component.scss'
