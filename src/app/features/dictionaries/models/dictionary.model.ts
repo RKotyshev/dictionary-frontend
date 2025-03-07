b4c802ab34beded1b90b97c52bdb54d3f44e3b9d
Roman Kotyshev <roman.kotyshev@yandex.ru>
2025-03-07 16:53:15 +0300

    DF-3: prepare base dictionary component usage (base table template and columns options directive)

--- src/app/features/dictionaries/models/dictionary.model.ts
+++ src/app/features/dictionaries/models/dictionary.model.ts
@@ -1,6 +1,13 @@
+import { ColumnOptions } from '../../../shared/models/column-options.model';
+
 export interface Dictionary {
   id: number;
   title: string;
   description: string;
   isFavorite: boolean;
+}
+
+export interface ColumnWithOptions {
+  columnName: string;
+  options: ColumnOptions;
 }
---
+++ src/app/shared/models/column-options.model.ts
@@ -0,0 +1,8 @@
+export interface ColumnOptions {
+  'width'?: string;
+  'height'?: string;
+  'min-width'?: string;
+  'min-height'?: string;
+  'max-width'?: string;
+  'max-height'?: string;
+}
